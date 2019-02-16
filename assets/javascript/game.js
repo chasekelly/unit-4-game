3625d1d1e25053f5a153a1df70ab6d21fa941516
Chase Kelly <chaserkelly3211@gmail.com>
2019-02-16 12:31:23 -0800

    Second Push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,96 +1,97 @@
-  $( document ).ready(function(){
-  // Generates random number to guess
-  var Random=Math.floor(Math.random()*102+19)
+$(document).ready(function(){
+// Generates random number to guess
+var TargetNumber = Math.floor(Math.random()*102+19)
   
-  // Display random number
-  $('#scoreToMatch').text(Random);
+// Display random number
+$('#scoreToMatch').text(TargetNumber);
   
-  //Generate random number for each crystal
-  var num1= Math.floor(Math.random()*12+1)
-  var num2= Math.floor(Math.random()*12+1)
-  var num3= Math.floor(Math.random()*12+1)
-  var num4= Math.floor(Math.random()*12+1)
+//Generate random number for each crystal
+var num1= Math.floor(Math.random()*12+1)
+var num2= Math.floor(Math.random()*12+1)
+var num3= Math.floor(Math.random()*12+1)
+var num4= Math.floor(Math.random()*12+1)
   
-  // Variables to keep track of wins, losses and total
-  var playerTotal= 0; 
-  var wins= 0;
-  var losses = 0;
-  
+// Variables to keep track of wins, losses and total
+var playerTotal = 0; 
+var wins = 0;
+var losses = 0;
 
 $('#wins').text(wins);
 $('#losses').text(losses);
-
-// Reset game
 function reset(){
-      Random=Math.floor(Math.random()*102+19);
-      console.log(Random)
-      $('#scoreToMatch').text(Random);
-      num1= Math.floor(Math.random()*12+1);
-      num2= Math.floor(Math.random()*12+1);
-      num3= Math.floor(Math.random()*12+1);
-      num4= Math.floor(Math.random()*12+1);
-      playerTotal= 0;
+      TargetNumber = Math.floor(Math.random()*102+19);
+      console.log(TargetNumber)
+      $('#scoreToMatch').text(TargetNumber);
+      num1 = Math.floor(Math.random()*12+1);
+      num2 = Math.floor(Math.random()*12+1);
+      num3 = Math.floor(Math.random()*12+1);
+      num4 = Math.floor(Math.random()*12+1);
+      playerTotal = 0;
       $('#totalScore').text(playerTotal);
       } 
-// Display wins
-function woohoo(){
+
+
+function winner(){
 alert("Congrats! You won!");
   wins++; 
   $('#wins').text(wins);
   reset();
-}
-// Display losses
+};
+
 function loser(){
 alert ("Sorry! You lose!");
   losses++;
   $('#losses').text(losses);
   reset()
-}
+};
+
 // Clicking crystals
-  $('.red').on ('click', function(){
+$('.red').on('click', function(){
     playerTotal = playerTotal + num1;
     console.log("New playerTotal= " + playerTotal);
     $('#totalScore').text(playerTotal); 
           //Win & lose conditions
-        if (playerTotal == Random){
-          woohoo();
+        if (playerTotal === TargetNumber){
+          winner();
         }
-        else if ( playerTotal > Random){
+        else if ( playerTotal > TargetNumber){
           loser();
         }   
-  })  
-  $('.blue').on ('click', function(){
+  });
+
+$('.blue').on('click', function(){
     playerTotal = playerTotal + num2;
     console.log("New playerTotal= " + playerTotal);
     $('#totalScore').text(playerTotal); 
-        if (playerTotal == Random){
-          woohoo();
+        if (playerTotal === TargetNumber){
+          winner();
         }
-        else if ( playerTotal > Random){
+        else if ( playerTotal > TargetNumber){
           loser();
         } 
-  })  
-  $('.yellow').on ('click', function(){
+  }); 
+
+$('.yellow').on('click', function(){
     playerTotal = playerTotal + num3;
     console.log("New playerTotal= " + playerTotal);
     $('#totalScore').text(playerTotal);
 
-          if (playerTotal == Random){
-          woohoo();
+          if (playerTotal === TargetNumber){
+          winner();
         }
-        else if ( playerTotal > Random){
+        else if ( playerTotal > TargetNumber){
           loser();
         } 
-  })  
-  $('.green').on ('click', function(){
+  });
+
+$('.green').on('click', function(){
     playerTotal = playerTotal + num4;
     console.log("New playerTotal= " + playerTotal);
     $('#totalScore').text(playerTotal); 
-      
-          if (playerTotal == Random){
-          woohoo();
+        if (playerTotal === TargetNumber){
+          winner();
         }
-        else if ( playerTotal > Random){
+        else if ( playerTotal > TargetNumber){
           loser();
         }
   });   
